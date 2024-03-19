136209a518ccb8cad096bf0acf6b3e75aeac033b
Eric Hansson <erichansson.united@gmail.com>
2024-03-19 13:46:56 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Author: Eric Hansson
+/*
+ * Step 1: Do HTTP request
+ * Step 2: Extracting and parsing the website code
+ * Step 3: Saving the relating data locally
+ */
+
+int main(int arc, char *argv[]){
+
+}
